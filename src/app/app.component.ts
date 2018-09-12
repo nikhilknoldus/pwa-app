c45687f09712959be60506db69f1d6e25f5abac7
Nikhil <nikhilkumar.net@gmail.com>
2018-09-12 13:58:26 +0530

    customized add to home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,60 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent implements OnInit {
 
-  constructor(public githubService:GithubService){}
+  constructor(public githubService: GithubService) { }
   title = 'app';
-  resData:any={};
+  resData: any = {};
+  deferredPrompt;
 
-  ngOnInit(){
-    setTimeout(()=>{
-    this.gethere()
-     }, 200)
+  ngOnInit() {
+    setTimeout(() => {
+      this.gethere();
+    }, 200);
+
+    /**
+     * for banner cusomization start
+     */
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      console.log('before installed');
+      e.preventDefault();
+      // Stash the event so it can be triggered later on the button event.
+      this.deferredPrompt = e;
+      return false;
+    });
+
+    // button click event to show the promt
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      console.log('display-mode is standalone');
+      alert('standalone');
+    }
+    window.addEventListener('appinstalled', (event) => {
+      alert('installed');
+    });
   }
+  openBanner() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            alert('User accepted the prompt');
+          } else {
+            alert('User dismissed the prompt');
+          }
+          this.deferredPrompt = null;
+        });
+    }
+  }
+  /**
+   * Banner customization end
+   */
 
 
-  gethere(){
-   this.githubService.getUserDetails().subscribe(val => this.resData = val);
+
+  gethere() {
+    this.githubService.getUserDetails().subscribe(val => this.resData = val);
   }
 }
