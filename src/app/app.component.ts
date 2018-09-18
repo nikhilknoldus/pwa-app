199d44ffe6804c123bedfd22b87e082304ae7b5e
Nikhil <nikhilkumar.net@gmail.com>
2018-09-18 14:51:59 +0530

    changed a2hs banner ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,70 +12,59 @@ export class AppComponent implements OnInit {
   constructor(public githubService: GithubService) { }
   title = 'app';
   resData: any = {};
-  deferredPrompt;
+  deferredPrompt:any;
 
   ngOnInit() {
     setTimeout(() => {
       this.gethere();
     }, 200);
 
-
+  
     window.addEventListener('beforeinstallprompt', function (e) {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       this.deferredPrompt = e;
-
       showAddToHomeScreen();
+      return false;
 
     });
 
+
+
     function showAddToHomeScreen() {
-
       var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
-
       a2hsBtn.style.display = "flex";
+      a2hsBtn.addEventListener("click", () => {
 
-      a2hsBtn.addEventListener("click", addToHomeScreen);
-
+        alert("called")
+        var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
+        // hide our user interface that shows our A2HS button
+        a2hsBtn.style.display = 'none';
+  
+        if (this.deferredPrompt) {
+          // Show the prompt
+          this.deferredPrompt.prompt();
+  
+          // Wait for the user to respond to the prompt
+          this.deferredPrompt.userChoice
+            .then(function (choiceResult) {
+  
+              if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+              } else {
+                console.log('User dismissed the A2HS prompt');
+              }
+              this.deferredPrompt = null;
+            });
+         }
+      });
     }
 
-    function addToHomeScreen() {
-
-      var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
-
-      // hide our user interface that shows our A2HS button
-      a2hsBtn.style.display = 'none';
-
-      if (this.deferredPrompt) {
-        // Show the prompt
-        this.deferredPrompt.prompt();
-
-        // Wait for the user to respond to the prompt
-        this.deferredPrompt.userChoice
-          .then(function (choiceResult) {
-
-            if (choiceResult.outcome === 'accepted') {
-              console.log('User accepted the A2HS prompt');
-            } else {
-              console.log('User dismissed the A2HS prompt');
-            }
-
-            this.deferredPrompt = null;
-
-          });
-
-      }
-
-    }
-
-    showAddToHomeScreen();
-
+    
     window.addEventListener('appinstalled', function (evt) {
       console.log('a2hs', 'installed');
     });
-
-
 
 
   }
