3ae2adaef09b4026fe3b6789e30ba9e2c58f2f8b
Nikhil <nikhilkumar.net@gmail.com>
2018-09-18 15:59:57 +0530

    changed a2hs banner ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   constructor(public githubService: GithubService) { }
   title = 'app';
   resData: any = {};
-  deferredPrompt:any;
+  deferredPrompt;
 
   ngOnInit() {
     setTimeout(() => {
@@ -29,39 +29,56 @@ export class AppComponent implements OnInit {
       return false;
     });
   }
-
-    showAddToHomeScreen() {
-      alert("called")
-      var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
-      a2hsBtn.style.display = "flex";
-      a2hsBtn.addEventListener("click", () => {
-
-        alert("called")
-        var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
-        // hide our user interface that shows our A2HS button
-        a2hsBtn.style.display = 'none';
-  
-        alert("before deferrerdPrompt if")
-        if (this.deferredPrompt) {
-          // Show the prompt
-          alert("before prompt")
-          this.deferredPrompt.prompt();
-          alert("after prompt")
-          
-          // Wait for the user to respond to the prompt
-          this.deferredPrompt.userChoice
-            .then(function (choiceResult) {
-              if (choiceResult.outcome === 'accepted') {
-                alert('User accepted the A2HS prompt');
-              } else {
-                alert('User dismissed the A2HS prompt');
-              }
-              this.deferredPrompt = null;
-            });
-         }
-         alert("outside if condition")
-      });
+   openBanner() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            alert('User accepted the prompt');
+          } else {
+            alert('User dismissed the prompt');
+          }
+          this.deferredPrompt = null;
+        });
     }
+  }
+
+    // showAddToHomeScreen() {
+    //   alert("called")
+    //   let _local = this;
+    //   var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
+    //   a2hsBtn.style.display = "flex";
+    //   a2hsBtn.addEventListener("click", function() {
+
+    //     alert("called")
+    //     var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
+    //     // hide our user interface that shows our A2HS button
+    //     a2hsBtn.style.display = 'none';
+  
+    //     alert("before deferrerdPrompt if")
+    //     alert("defferedPrompt value"+ _local.deferredPrompt)
+    //     if (_local.deferredPrompt) {
+    //       // Show the prompt
+    //       alert("before prompt")
+    //       _local.deferredPrompt.prompt();
+    //       alert("after prompt")
+          
+    //       // Wait for the user to respond to the prompt
+    //       _local.deferredPrompt.userChoice
+    //         .then(function (choiceResult) {
+    //           if (choiceResult.outcome === 'accepted') {
+    //             alert('User accepted the A2HS prompt');
+    //           } else {
+    //             alert('User dismissed the A2HS prompt');
+    //           }
+    //           this.deferredPrompt = null;
+    //         });
+    //      }
+    //      alert("outside if condition")
+    //   });
+    // }
 
 
     // window.addEventListener('appinstalled', function (evt) {
