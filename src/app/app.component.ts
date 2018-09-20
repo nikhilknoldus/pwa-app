b1fee26fae8f5e6817e1b89d9ba92d40a09dd2eb
Nikhil <nikhilkumar.net@gmail.com>
2018-09-20 12:54:44 +0530

    banner updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,64 +21,69 @@ export class AppComponent implements OnInit {
 
   
     window.addEventListener('beforeinstallprompt', function (e) {
+      alert("asdf");
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       this.deferredPrompt = e;
+      alert("deffered value"+ this.defferedPrompt);
       // showAddToHomeScreen();
+      // this.openBanner();
       return false;
     });
   }
-   openBanner() {
-    if (this.deferredPrompt) {
-      this.deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      this.deferredPrompt.userChoice
-        .then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            alert('User accepted the prompt');
-          } else {
-            alert('User dismissed the prompt');
-          }
-          this.deferredPrompt = null;
-        });
-    }
-  }
+  // public openBanner() {
+  //   // alert("in openAlertBanner method")
+  //   if (this.deferredPrompt) {
+  //     this.deferredPrompt.prompt();
+  //     // Wait for the user to respond to the prompt
+  //     this.deferredPrompt.userChoice
+  //       .then((choiceResult) => {
+  //         if (choiceResult.outcome === 'accepted') {
+  //           alert('User accepted the prompt');
+  //         } else {
+  //           alert('User dismissed the prompt');
+  //         }
+  //         this.deferredPrompt = null;
+  //       });
+  //   }
+  // }
 
-    // showAddToHomeScreen() {
-    //   alert("called")
-    //   let _local = this;
-    //   var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
-    //   a2hsBtn.style.display = "flex";
-    //   a2hsBtn.addEventListener("click", function() {
+    showAddToHomeScreen() {
+      alert("called")
+      let _local = this;
+      var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
+      a2hsBtn.style.display = "flex";
+      a2hsBtn.addEventListener("click", () => {
 
-    //     alert("called")
-    //     var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
-    //     // hide our user interface that shows our A2HS button
-    //     a2hsBtn.style.display = 'none';
+        alert("called")
+        var a2hsBtn = <HTMLElement>document.querySelector(".ad2hs-prompt");
+        // hide our user interface that shows our A2HS button
+        a2hsBtn.style.display = 'none';
   
-    //     alert("before deferrerdPrompt if")
-    //     alert("defferedPrompt value"+ _local.deferredPrompt)
-    //     if (_local.deferredPrompt) {
-    //       // Show the prompt
-    //       alert("before prompt")
-    //       _local.deferredPrompt.prompt();
-    //       alert("after prompt")
+        alert("before deferrerdPrompt if")
+        alert("defferedPrompt value"+ this.deferredPrompt)
+        if (this.deferredPrompt) {
+          // Show the prompt
+          alert("before prompt")
           
-    //       // Wait for the user to respond to the prompt
-    //       _local.deferredPrompt.userChoice
-    //         .then(function (choiceResult) {
-    //           if (choiceResult.outcome === 'accepted') {
-    //             alert('User accepted the A2HS prompt');
-    //           } else {
-    //             alert('User dismissed the A2HS prompt');
-    //           }
-    //           this.deferredPrompt = null;
-    //         });
-    //      }
-    //      alert("outside if condition")
-    //   });
-    // }
+          this.deferredPrompt.prompt();
+          alert("after prompt")
+          
+          // Wait for the user to respond to the prompt
+          this.deferredPrompt.userChoice
+            .then(function (choiceResult) {
+              if (choiceResult.outcome === 'accepted') {
+                alert('User accepted the A2HS prompt');
+              } else {
+                alert('User dismissed the A2HS prompt');
+              }
+              this.deferredPrompt = null;
+            });
+         }
+         alert("outside if condition")
+      });
+    }
 
 
     // window.addEventListener('appinstalled', function (evt) {
